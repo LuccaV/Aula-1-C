03d76e5c72a909a5a18f26d4bbc97eff22190dbf
LuccaV <80118511+LuccaV@users.noreply.github.com>
2025-02-13 11:41:56 +0000

    variavel

---
+++ Pasta/aula_dois.c
@@ -0,0 +1,9 @@
+int main() {
+    int n1 = 5;
+    double n2 = 3.1801;
+    char letra = 'a';
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+    printf("letra = %c\n", letra);
+    return 0;
+}
